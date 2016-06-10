a55da5779ed7b9458a0e5badfa1e7add2495b927
Stefan Reinhold <sre@informatik.uni-kiel.de>
2016-06-10 13:01:59 +0200

    Addes support for 2d float vector uniforms

--- Source/ShaderProgram.h
+++ Source/ShaderProgram.h
@@ -64,6 +64,13 @@ public:
     return *this;
   }
 
+  UniformProxy const &operator=(Eigen::Vector2f const &v) const noexcept {
+    assertGL("Precondition violation");
+    glUniform2fv(location_, 1, v.data());
+    assertGL("Failed to upload uniform");
+    return *this;
+  }
+
   UniformProxy const &operator=(Eigen::Matrix4f const &m) const noexcept {
     assertGL("Precondition violation");
     glUniformMatrix4fv(location_, 1, false, m.data());
