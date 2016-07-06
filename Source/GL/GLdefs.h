6f6c03847ecdde8b5d7495bb29aeff51eeb63e11
Stefan Reinhold <stefan@sreinhold.com>
2016-07-06 15:49:13 +0200

    fix OpenGL 4.1 compatibility

--- Source/GL/GLdefs.h
+++ Source/GL/GLdefs.h
@@ -3,3 +3,11 @@
 #define GL_GLEXT_PROTOTYPES
 #include <GLFW/glfw3.h>
 
+#ifndef GL_ZERO_TO_ONE
+#define GL_ZERO_TO_ONE 0x935F
+#endif
+
+#if !defined(GL_ARB_clip_control) && !defined(GL_ARB_clip_control_PROTOTYPE)
+#define GL_ARB_clip_control_PROTOTYPE
+inline void glClipControl(GLenum, GLenum){};
+#endif
