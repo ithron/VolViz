4c1eca37aefd66a2039ede93246b16dd9d88883d
Stefan Reinhold <sre@informatik.uni-kiel.de>
2016-09-20 08:39:32 +0200

    Add GeometryFactory class

---
+++ Source/GeometryFactory.cpp
@@ -0,0 +1,17 @@
+#include "GeometryFactory.h"
+
+#include "VisualizerImpl.h"
+
+namespace VolViz {
+namespace Private_ {
+
+GeometryFactory::GeometryFactory(VisualizerImpl &visualizer)
+    : visualizer_(visualizer) {}
+
+GeometryFactory::GeometryPtr
+GeometryFactory::create(AxisAlignedPlaneDescriptor const &descriptor) {
+  return std::make_unique<AxisAlignedPlane>(descriptor, visualizer_);
+}
+
+} // namespace Private_
+} // namespace VolViz
