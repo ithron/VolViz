2a401f41a5a1ada5ef49b28c4202242921cc4ec5
Stefan Reinhold <sre@informatik.uni-kiel.de>
2016-09-22 16:52:59 +0200

    Fix doc generation script

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -71,9 +71,9 @@ echo "" > .nojekyll
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
-# doxygen $DOXYFILE 2>&1 | tee doxygen.log
-make -C .. doc 2>&1 | tee doxygen.log
-mv ../html ./
+doxygen $DOXYFILE 2>&1 | tee doxygen.log
+# make -C .. doc 2>&1 | tee doxygen.log
+# mv ../html ./
 
 ################################################################################
 ##### Upload the documentation to the gh-pages branch of the repository.   #####
