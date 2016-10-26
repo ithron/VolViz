bdc4857087e1210a677665085c23940cbef51f37
Stefan Reinhold <sre@informatik.uni-kiel.de>
2016-10-26 17:27:52 +0200

    Prefix all targets with VolViz::

--- Dependencies/cmake/ImportConcurrentqueue.cmake
+++ Dependencies/cmake/ImportConcurrentqueue.cmake
@@ -1,15 +1,15 @@
 cmake_minimum_required(VERSION 3.2 FATAL_ERROR)
 
-add_library(ConcurrentQueue INTERFACE IMPORTED)
+add_library(VolViz::ConcurrentQueue INTERFACE IMPORTED)
 if("${CMAKE_GENERATOR}" STREQUAL "Xcode")
-  set_property(TARGET ConcurrentQueue PROPERTY INTERFACE_COMPILE_OPTIONS
+  set_property(TARGET VolViz::ConcurrentQueue PROPERTY INTERFACE_COMPILE_OPTIONS
     -isystem${DEPENDENCIES_DIR}/concurrentqueue
   )
 else()
-  set_property(TARGET ConcurrentQueue PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+  set_property(TARGET VolViz::ConcurrentQueue PROPERTY INTERFACE_INCLUDE_DIRECTORIES
     ${DEPENDENCIES_DIR}/concurrentqueue
   )
-  set_property(TARGET ConcurrentQueue PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
+  set_property(TARGET VolViz::ConcurrentQueue PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
     ${DEPENDENCIES_DIR}/concurrentqueue
   )
 endif()
--- Dependencies/cmake/ImportPhysUnits.cmake
+++ Dependencies/cmake/ImportPhysUnits.cmake
@@ -1,15 +1,15 @@
 cmake_minimum_required(VERSION 3.2 FATAL_ERROR)
 
-add_library(PhysUnits INTERFACE IMPORTED)
+add_library(VolViz::PhysUnits INTERFACE IMPORTED)
 if(XCODE)
-  set_property(TARGET PhysUnits PROPERTY INTERFACE_COMPILE_OPTIONS
+  set_property(TARGET VolViz::PhysUnits PROPERTY INTERFACE_COMPILE_OPTIONS
     -isystem${DEPENDENCIES_DIR}/PhysUnits
   )
 else()
-  set_property(TARGET PhysUnits PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+  set_property(TARGET VolViz::PhysUnits PROPERTY INTERFACE_INCLUDE_DIRECTORIES
     ${DEPENDENCIES_DIR}/PhysUnits
   )
-  set_property(TARGET PhysUnits PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
+  set_property(TARGET VolViz::PhysUnits PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
     ${DEPENDENCIES_DIR}/PhysUnits
   )
 endif()
