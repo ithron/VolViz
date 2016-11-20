198da64ba5783bdd4f0c9cf91be6600f620020f2
Stefan Reinhold <stefan@sreinhold.com>
2016-11-20 19:08:50 +0100

    Add a script to configure dependencies for Xcode project build

---
+++ configDependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p Dependencies/build
+
+cd Dependencies/build
+
+# Build GLFW
+mkdir -p glfw
+cd glfw
+cmake ../../glfw -DCMAKE_BUILD_TYPE=Releast -GXcode
+cd ..
+
+# Build googletest
+mkdir -p googletest
+cd googletest
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_GTEST=On -DBUILD_GMOCK=Off -GXcode ../../googletest
