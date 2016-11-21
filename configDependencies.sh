d3907506e64f72dbf8eddef4cc7f9f3848ae5081
Stefan Reinhold <stefan@sreinhold.com>
2016-11-21 13:07:02 +0100

    Remove googletest dependencie, since it's not used

--- configDependencies.sh
+++ configDependencies.sh
@@ -9,10 +9,10 @@ cd Dependencies/build
 # Build GLFW
 mkdir -p glfw
 cd glfw
-cmake ../../glfw -DCMAKE_BUILD_TYPE=Releast -GXcode
+cmake ../../glfw -DCMAKE_BUILD_TYPE=Release -GXcode
 cd ..
 
-# Build googletest
-mkdir -p googletest
-cd googletest
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_GTEST=On -DBUILD_GMOCK=Off -GXcode ../../googletest
+# # Build googletest
+# mkdir -p googletest
+# cd googletest
+# cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_GTEST=On -DBUILD_GMOCK=Off -GXcode ../../googletest
