a051f2198cc7a25da992461fa816f0ceee5dcae2
Travis CI <travis@travis-ci.org>
2016-12-16 09:41:13 +0000

    Deploy code docs to GitHub Pages Travis build: 144
    Commit: ef2bd1dc4f8d975a4bd374b165abe66bbe10670a

--- html/search/classes_0.js
+++ html/search/classes_0.js
@@ -2,10 +2,12 @@ var searchData=
 [
   ['atomiccache',['AtomicCache',['../classVolViz_1_1AtomicCache.html',1,'VolViz']]],
   ['atomiccache_3c_20angle_20_3e',['AtomicCache&lt; Angle &gt;',['../classVolViz_1_1AtomicCache.html',1,'VolViz']]],
+  ['atomiccache_3c_20color_20_3e',['AtomicCache&lt; Color &gt;',['../classVolViz_1_1AtomicCache.html',1,'VolViz']]],
   ['atomiccache_3c_20length_20_3e',['AtomicCache&lt; Length &gt;',['../classVolViz_1_1AtomicCache.html',1,'VolViz']]],
   ['atomiccache_3c_20matrix4_20_3e',['AtomicCache&lt; Matrix4 &gt;',['../classVolViz_1_1AtomicCache.html',1,'VolViz']]],
   ['atomicwrapper',['AtomicWrapper',['../classVolViz_1_1AtomicWrapper.html',1,'VolViz']]],
   ['atomicwrapper_3c_20angle_20_3e',['AtomicWrapper&lt; Angle &gt;',['../classVolViz_1_1AtomicWrapper.html',1,'VolViz']]],
+  ['atomicwrapper_3c_20color_20_3e',['AtomicWrapper&lt; Color &gt;',['../classVolViz_1_1AtomicWrapper.html',1,'VolViz']]],
   ['atomicwrapper_3c_20float_20_3e',['AtomicWrapper&lt; float &gt;',['../classVolViz_1_1AtomicWrapper.html',1,'VolViz']]],
   ['atomicwrapper_3c_20length_20_3e',['AtomicWrapper&lt; Length &gt;',['../classVolViz_1_1AtomicWrapper.html',1,'VolViz']]],
   ['atomicwrapper_3c_20orientation_20_3e',['AtomicWrapper&lt; Orientation &gt;',['../classVolViz_1_1AtomicWrapper.html',1,'VolViz']]],
--- html/search/variables_1.js
+++ html/search/variables_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['backgroundcolor',['backgroundColor',['../classVolViz_1_1Visualizer.html#a5df55244eb852447082ecf36a032df52',1,'VolViz::Visualizer']]],
   ['bboxgeometryshadersrc',['bboxGeometryShaderSrc',['../namespaceVolViz_1_1Private___1_1GL_1_1Shaders.html#aaead6a92999f8458a090e040186c44b9',1,'VolViz::Private_::GL::Shaders']]],
   ['beforeaction',['beforeAction',['../classVolViz_1_1SetAndNotifyPolicy.html#a3b8e1358a21e92bd8b97b88cb90d61db',1,'VolViz::SetAndNotifyPolicy']]],
   ['buffers',['buffers',['../structVolViz_1_1Private___1_1VisualizerImpl_1_1SelectionBuffer.html#a56c4d88b531fb2d5b6ed40624c67aa60',1,'VolViz::Private_::VisualizerImpl::SelectionBuffer']]]
