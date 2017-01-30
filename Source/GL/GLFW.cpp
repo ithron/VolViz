59692e4f8b9843f633839199983c19e6890fe076
Stefan Reinhold <sre@informatik.uni-kiel.de>
2017-01-30 09:34:21 +0100

    Use glad to load dynamic OpenGL functions

--- Source/GL/GLFW.cpp
+++ Source/GL/GLFW.cpp
@@ -45,6 +45,10 @@ GLFW::GLFW(std::string title, std::size_t width, std::size_t height) {
     throw std::runtime_error("Failed to create GLFW window");
   }
 
+  if (!gladLoadGL()) {
+    throw std::runtime_error("Failed to load OpenGL functions");
+  }
+
   glfwSetWindowUserPointer(window, this);
 
   // setup key callback handler
