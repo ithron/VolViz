76e917d69df8d10f74e9a5a40b442fd03d6529d9
Stefan Reinhold <sre@informatik.uni-kiel.de>
2017-02-01 15:10:15 +0100

    Remove no longer required work arounds

--- Source/GL/GLdefs.h
+++ Source/GL/GLdefs.h
@@ -13,7 +13,3 @@
 #define GL_ZERO_TO_ONE 0x935F
 #endif
 
-#if !defined(GL_ARB_clip_control) && !defined(GL_ARB_clip_control_PROTOTYPE)
-#define GL_ARB_clip_control_PROTOTYPE
-inline void glClipControl(GLenum, GLenum) {}
-#endif
--- Source/GL/Workarounds.cpp
+++
@@ -1,7 +0,0 @@
-#include "config.h"
-#include "GLdefs.h"
-
-
-#ifdef FIX_MISSING_GL_IMPLEMENTATIONS
-void glClipControl(GLenum, GLenum) {}
-#endif
