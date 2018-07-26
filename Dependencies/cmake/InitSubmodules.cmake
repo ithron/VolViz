f47465170edbabf84f93eb4c86ba1b2a1684673b
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-07-26 15:42:05 +0200

    Remove debug output

--- Dependencies/cmake/InitSubmodules.cmake
+++ Dependencies/cmake/InitSubmodules.cmake
@@ -4,13 +4,10 @@ find_package(Git REQUIRED)
 
 message("===== Init submodules =====")
 
-message("++++ Running '${GIT_EXECUTABLE} submodule update --init' in ${CMAKE_CURRENT_SOURCE_DIR}")
 execute_process(
   COMMAND "${GIT_EXECUTABLE}" submodule update --init
   WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
 )
-
-
 
 message("===== Submodules initialized =====")
 
